76bb0ab3cd9ee720e93deef700da4ecc0bad1a74
Vika Shoynkhorova <shoynkhorova@ya.ru>
2023-01-20 00:19:55 +0300

    merged with develop

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,11 @@
+// выпадающее меню
+const menuIcon = document.querySelector('.header__menu-icon');
+const headerSpan = document.querySelectorAll('.header__span')
+
+menuIcon.addEventListener("click", function(e) {
+  headerSpan.classList.toggle('_activ');
+})
+
 // блок Advantages (главное в работе)
 
 const btnsAccordions = document.querySelectorAll('.advantages__button');
